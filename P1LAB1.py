b5221e6f16b6e15e2f4b616bb5c8203a4273c37e
notorieti <smithd4829@student.faytechcc.edu>
2025-02-06 14:21:19 -0500

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+# Davin Smith
+# 2-6-2025
+# P1LAB1
+# This program will demo python code.
+
+fname = input("Enter your first name:")
+
+lname = input("Enter your last name:")
+print("Hello,"+str(fname)+ ""+ str(lname)+"! Welcome to CTI-110")
+
+print("Hello,",fname,lname,"Welcome to CTI-110")
